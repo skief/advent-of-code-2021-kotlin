a70d3cf3ccae25fbae01664a3d3dd8fc3e5b3e59
Simon Kiefhaber <simon.kiefhaber@outlook.de>
2021-12-01 21:33:40 +0100

    day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,25 @@
 fun main() {
+    fun countAscending(input: List<String>, windowSize: Int): Int{
+        return input
+            .map(String::toInt)
+            .windowed(size = windowSize, step = 1)
+            .map { w->w.sum() }
+            .zipWithNext()
+            .filter { (i, j) -> i < j }
+            .size
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return countAscending(input, 1)
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return countAscending(input, 3)
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
 
     val input = readInput("Day01")
     println(part1(input))
